362a50a954d690ec947ba4ddcef031fed3f9a185
Jake Borromeo <jakeb1050@gmail.com>
2022-01-30 21:54:35 -0700

    Fixed express file

--- builder.js
+++ builder.js
@@ -10,7 +10,7 @@ const isDev = process.env.NODE_ENV !== 'production'
  * @link https://www.npmjs.com/package/live-server#usage-from-node
  */
 const serverParams = {
-  port: 8181, // Set the server port. Defaults to 8080.
+  port: 5000, // Set the server port. Defaults to 8080.
   root: 'public', // Set root directory that's being served. Defaults to cwd.
   open: true // When false, it won't load your browser by default.
   // host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
